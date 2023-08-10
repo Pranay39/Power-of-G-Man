378f348f11d9d1f7a23f94c2bfd44ec91cd97df4
Pranay Patil <pranay.patil@seligentconsulting.com>
2023-08-10 22:11:37 +0530

    testing branch v2 working

--- src/main/java/com/example/geektrust/service/XYPowerCalculatorService.java
+++ src/main/java/com/example/geektrust/service/XYPowerCalculatorService.java
@@ -4,22 +4,22 @@ import com.example.geektrust.utils.MoveConstants;
 
 import java.util.List;
 
-public class XYPowerCalculatorService implements PowerCalculatorService {
-    @Override
-    public int calculatePower(int movements, List<Character> directions, String sourceDirection) {
-        // Calculate power for Axis XY based on provided parameters
-        int powerSpent = 0;
+    public class XYPowerCalculatorService implements PowerCalculatorService {
+        @Override
+        public int calculatePower(int movements, List<Character> directions, String sourceDirection) {
+            // Calculate power for Axis XY based on provided parameters
+            int powerSpent = 0;
 
-        // Calculate power spent on movements
-        int movementPower = movements * MoveConstants.MOVE_COST;
-        powerSpent += movementPower;
+            // Calculate power spent on movements
+            int movementPower = movements * MoveConstants.MOVE_COST;
+            powerSpent += movementPower;
 
-        // Check if the source direction is contained in the given directions
-        if (directions.contains(sourceDirection.charAt(0))) {
-            powerSpent += MoveConstants.TURN_COST;
-        }else{
-            powerSpent += MoveConstants.TURN_COST * MoveConstants.TWO_TURNS;
+            // Check if the source direction is contained in the given directions
+            if (directions.contains(sourceDirection.charAt(0))) {
+                powerSpent += MoveConstants.TURN_COST;
+            }else{
+                powerSpent += MoveConstants.TURN_COST * MoveConstants.TWO_TURNS;
+            }
+            return MoveConstants.INITIAL_POWER - powerSpent;
         }
-        return MoveConstants.INITIAL_POWER - powerSpent;
     }
-}
--- src/main/java/com/example/geektrust/service/YPowerCalculatorService.java
+++ src/main/java/com/example/geektrust/service/YPowerCalculatorService.java
@@ -5,31 +5,31 @@ import com.example.geektrust.utils.MoveConstants;
 
 import java.util.List;
 
-public class YPowerCalculatorService implements PowerCalculatorService {
-    @Override
-    public int calculatePower(int movements, List<Character> directions, String sourceDirection) {
-        // Calculate power for Axis Y based on provided parameters
-        int powerSpent = 0;
+    public class YPowerCalculatorService implements PowerCalculatorService {
+        @Override
+        public int calculatePower(int movements, List<Character> directions, String sourceDirection) {
+            // Calculate power for Axis Y based on provided parameters
+            int powerSpent = 0;
 
-        // Calculate power spent on movements
-        int movementPower = movements * MoveConstants.MOVE_COST;
-        powerSpent += movementPower;
+            // Calculate power spent on movements
+            int movementPower = movements * MoveConstants.MOVE_COST;
+            powerSpent += movementPower;
 
-        // Check if the source direction is contained in the given directions
-        if (directions.contains(sourceDirection.charAt(0))) {
-            return MoveConstants.INITIAL_POWER - movementPower;
+            // Check if the source direction is contained in the given directions
+            if (directions.contains(sourceDirection.charAt(0))) {
+                return MoveConstants.INITIAL_POWER - movementPower;
+            }
+
+            // Check if source direction is EAST or WEST
+            if (sourceDirection.equals(String.valueOf(Direction.EAST.getSymbol())) ||
+                    sourceDirection.equals(String.valueOf(Direction.WEST.getSymbol()))) {
+                powerSpent += MoveConstants.TURN_COST;
+            }else{
+                powerSpent += MoveConstants.TURN_COST * MoveConstants.TWO_TURNS;
+
+            }
+
+            return MoveConstants.INITIAL_POWER - powerSpent;
         }
-
-        // Check if source direction is EAST or WEST
-        if (sourceDirection.equals(String.valueOf(Direction.EAST.getSymbol())) ||
-                sourceDirection.equals(String.valueOf(Direction.WEST.getSymbol()))) {
-            powerSpent += MoveConstants.TURN_COST;
-        }else{
-            powerSpent += MoveConstants.TURN_COST * MoveConstants.TWO_TURNS;
-
-        }
-
-        return MoveConstants.INITIAL_POWER - powerSpent;
     }
-}
 
